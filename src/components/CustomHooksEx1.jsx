2eb5dac0c8921439354e7abe9343b0c7564f36a8
Emran <emranitmbstu@gmail.com>
2023-10-17 01:12:40 +0600

    new

--- src/components/CustomHooksEx1.jsx
+++ src/components/CustomHooksEx1.jsx
@@ -3,13 +3,13 @@ import useFetch from "../customHook/useFetch";
 function CustomHooksEx1() {
     const url = "https://jsonplaceholder.typicode.com/users";
     const res = useFetch(url, {});
-    console.log(res);
+    console.log('why double')
     if (res.loadin)
         return <h3>Loading.....</h3>
     const items = res.data;
     return (
         <div>
-            <h1 className="text-center taxt-bold">It's a bad problem</h1>
+            <h1 className="text-center">It's a bad problem</h1>
             <h3 className="mb-5 text-center text-3xl">Fetch data from an api in react</h3>
             <div className="container text-center">
                 {items.map((item) => (
